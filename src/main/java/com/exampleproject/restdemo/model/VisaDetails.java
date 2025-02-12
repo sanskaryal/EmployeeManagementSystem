02b8d757ab65c14b8ab7ec69d623844c96ac688f
Anjila <anjeelaneupane@gmail.com>
2025-02-12 13:15:45 -0600

    git ignore added

---
+++ src/main/java/com/exampleproject/restdemo/model/VisaDetails.java
@@ -0,0 +1,41 @@
+package com.exampleproject.restdemo.mode;
+
+public class VisaDetails {
+    private String visaStatus;
+    private String startDate;
+    private String endDate;
+    private String document;
+
+    public String getVisaStatus() {
+        return visaStatus;
+    }
+
+    public void setVisaStatus(String visaStatus) {
+        this.visaStatus = visaStatus;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+}
