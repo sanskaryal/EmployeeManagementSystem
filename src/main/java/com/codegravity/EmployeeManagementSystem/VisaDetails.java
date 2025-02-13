5f33352bffd15d64cd937aafb2b16ddc42771e07
saugatacharya98 <saugatacharya022@gmail.com>
2025-02-13 11:51:28 -0600

    Changes

---
+++ src/main/java/com/codegravity/EmployeeManagementSystem/VisaDetails.java
@@ -0,0 +1,42 @@
+package com.codegravity.EmployeeManagementSystem;
+
+public class VisaDetails {
+    private String visaStatus;
+    private String startDate;
+    private String endDate;
+    private String document;
+
+    public String getVisaStatus() {
+        return visaStatus;
+    }
+
+    public void setVisaStatus(String visaStatus) {
+        this.visaStatus = visaStatus;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+
+}
